ec064b210d041d8c2f7cc33e403bf4e5b036ddab
sum-bocfob <43840414+sum-bocfob@users.noreply.github.com>
2025-04-13 11:26:45 +0900

    About Us完成 / Works 見出し完成、タブガワだけ完成、movies完成

--- js/main.js
+++ js/main.js
@@ -13,3 +13,17 @@ const swiper = new Swiper(".swiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+$(function () {
+    // クリックで動画再生
+    const video_wrapper = $(".works__video-wrapper");
+    video_wrapper.on("click", function () {
+        const video = $(this).children("video").get(0);
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+        $(this).toggleClass("playing");
+    });
+});
